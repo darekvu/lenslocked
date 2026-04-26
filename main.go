6ed382ab0052772f198f77e87664a956e4a3c6dc
Quang Du Vu (Darek) <vuquang.dd@gmail.com>
2026-04-26 22:21:20 +0200

    refactor our code for temp[late and views

--- main.go
+++ main.go
@@ -2,44 +2,39 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 
+	"github.com/darekvu/lenslocked/views"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func execTemplate(w http.ResponseWriter, filePath string) {
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	tpl, err := template.ParseFiles(filePath)
+func executeTemplate(w http.ResponseWriter, filePath string) {
+	tpl, err := views.Parse(filePath)
 	if err != nil {
 		log.Printf("Error parsing template: %v", err)
-		http.Error(w, "There was an error parsing the template.", http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	err = tpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("Error executing template: %v", err)
-		http.Error(w, "There was an error executing the template.", http.StatusInternalServerError)
-		return
-	}
+	tpl.Execute(w, nil)
 }
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	tplPath := filepath.Join("templates", "home.gohtml")
-	execTemplate(w, tplPath)
+	executeTemplate(w, tplPath)
 
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
 	tplPath := filepath.Join("templates", "contact.gohtml ")
-	execTemplate(w, tplPath)
+	executeTemplate(w, tplPath)
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
 	tplPath := filepath.Join("templates", "faq.gohtml")
-	execTemplate(w, tplPath)
+	executeTemplate(w, tplPath)
 }
 func main() {
 	r := chi.NewRouter()
@@ -47,6 +42,9 @@ func main() {
 	r.Get("/", homeHandler)
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.Error(w, "Not found", http.StatusNotFound)
+	})
 	//r.Get("/faq", faqHandler)
 	fmt.Println("Starting Server on port 3000...")
 	http.ListenAndServe("127.0.0.1:3000", r)
---
+++ views/template.go
@@ -0,0 +1,31 @@
+package views
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+type Template struct {
+	HTMLTlp *template.Template
+}
+
+func Parse(filepath string) (Template, error) {
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		return Template{}, fmt.Errorf("parse template file error: %s", err)
+	}
+	return Template{
+		HTMLTlp: tpl,
+	}, nil
+}
+func (t Template) Execute(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	err := t.HTMLTlp.Execute(w, data)
+	if err != nil {
+		log.Printf("Error executing template: %v", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
